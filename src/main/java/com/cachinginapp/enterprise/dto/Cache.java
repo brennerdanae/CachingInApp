6ed1fc19cbeda1635c368feb1bd7ad42cc3e6771
momoney13 <64868507+momoney13@users.noreply.github.com>
2022-09-24 10:13:20 -0400

    Added DTO with Lombok

---
+++ src/main/java/com/cachinginapp/enterprise/dto/Cache.java
@@ -0,0 +1,12 @@
+package com.cachinginapp.enterprise.dto;
+
+import lombok.Data;
+
+public @Data
+class Cache {
+    private int cacheID;
+    private String latitude;
+    private String longitude;
+    private String description;
+
+}
