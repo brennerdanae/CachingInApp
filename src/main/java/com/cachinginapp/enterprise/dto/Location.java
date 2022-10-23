54891b7cf0ade25191bfb780a445fe0919c87955
momoney13 <64868507+momoney13@users.noreply.github.com>
2022-10-23 16:56:22 -0400

    Added SerializedName strings/ints

--- src/main/java/com/cachinginapp/enterprise/dto/Location.java
+++ src/main/java/com/cachinginapp/enterprise/dto/Location.java
@@ -3,9 +3,18 @@ package com.cachinginapp.enterprise.dto;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
-import java.util.List;
-
 public @Data class Location {
-
+    @SerializedName("State")
+    private String state;
+    @SerializedName("County")
+    private String county;
+    @SerializedName("City")
+    private String city;
+    @SerializedName("Zip-Code")
+    private Integer zip;
+    @SerializedName("Longitude")
+    private Integer longitude;
+    @SerializedName("Latitude")
+    private Integer latitude;
 
 }
