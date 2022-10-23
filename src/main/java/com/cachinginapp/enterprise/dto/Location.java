baf7f908453f0b226db55298af14802d7d89ab7d
momoney13 <64868507+momoney13@users.noreply.github.com>
2022-10-23 16:40:07 -0400

    Added gson and created Location.Java

---
+++ src/main/java/com/cachinginapp/enterprise/dto/Location.java
@@ -0,0 +1,11 @@
+package com.cachinginapp.enterprise.dto;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+import java.util.List;
+
+public @Data class Location {
+
+
+}
