b42facdcf748a2625fd6e952a7cad4cbf9e5bf6e
momoney13 <64868507+momoney13@users.noreply.github.com>
2022-11-13 20:18:14 -0500

    Enabled Caching

--- src/main/java/com/cachinginapp/enterprise/EnterpriseApplication.java
+++ src/main/java/com/cachinginapp/enterprise/EnterpriseApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCaching
+
 public class EnterpriseApplication {
 
 	public static void main(String[] args) {
