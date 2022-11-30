27777b015dd4cc2a2250a3be3c9945018010f828
momoney13 <64868507+momoney13@users.noreply.github.com>
2022-11-30 18:50:39 -0500

    revert

--- src/main/java/com/cachinginapp/enterprise/CachingInController.java
+++ src/main/java/com/cachinginapp/enterprise/CachingInController.java
@@ -157,7 +157,7 @@ public class CachingInController {
      */
     @GetMapping("/cachepoints")
     public ResponseEntity searchCaches(@RequestParam(value="searchTerm", required = false, defaultValue ="None") String searchTerm){
-        ResponseEntity createCache;
+        ResponseEntity.AddObject("caches", cache);
         return new ResponseEntity(HttpStatus.OK);
     }
 
